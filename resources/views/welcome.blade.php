aaa378069c7290036643ab7433657c750ff57d6a
jason <hengweno@163.com>
2018-10-19 17:58:12 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Auto
+                    Laravel Aut
                 </div>
 
                 <div class="links">
