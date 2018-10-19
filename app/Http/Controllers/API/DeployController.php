83b55ba4d6e2dab6bf6eda5d37cac6bc4caeed4d
jason <hengweno@163.com>
2018-10-19 18:17:52 +0800

    test

--- app/Http/Controllers/API/DeployController.php
+++ app/Http/Controllers/API/DeployController.php
@@ -11,23 +11,27 @@ class DeployController extends Controller
 {
     public function deploy(Request $request)
     {
-        $githubPayload = $request->getContent();
-        $githubHash = $request->header('X-Hub-Signature');
-        $localToken = config('app.deploy_secret');
-        $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
-        Log::info($githubHash);
-        Log::info($localHash);
-        //test again
-        Log::info(hash_equals($githubHash, $localHash));
+        try {
+            $githubPayload = $request->getContent();
+            $githubHash = $request->header('X-Hub-Signature');
+            $localToken = config('app.deploy_secret');
+            $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+            Log::info($githubHash);
+            Log::info($localHash);
+            //test again
+            Log::info(hash_equals($githubHash, $localHash));
 
 
-        if (hash_equals($githubHash, $localHash)) {
-            $root_path = base_path();
-            Log::info('cd ' . $root_path . '/scripts; ./deploy.sh');
-            $process = new Process('cd ' . $root_path . '/scripts; ./deploy.sh');
-            $process->run(function ($type, $buffer) {
-                echo $buffer;
-            });
+            if (hash_equals($githubHash, $localHash)) {
+                $root_path = base_path();
+                Log::info('cd ' . $root_path . '/scripts; ./deploy.sh');
+                $process = new Process('cd ' . $root_path . '/scripts; ./deploy.sh');
+                $process->run(function ($type, $buffer) {
+                    echo $buffer;
+                });
+            }
+        } catch (\Exception $exception) {
+            Log::error($exception->getMessage());
         }
     }
 }
