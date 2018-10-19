5deb1afce5af738d4ac744a2a800ef1314099fb3
jason <hengweno@163.com>
2018-10-19 18:23:21 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Auto Deploy Againssss
+                    Laravel Auto Deploy Againsssssssssssssss
                 </div>
 
                 <div class="links">
