fb0965aa23893b358492c061ad341f3b66934bc0
jason <hengweno@163.com>
2018-10-19 18:15:57 +0800

    test again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Auto Deploys
+                    Laravel Auto Deploy Again
                 </div>
 
                 <div class="links">
