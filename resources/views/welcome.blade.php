fe591de1e061dce05b6df74d0b99676919190df0
jason <hengweno@163.com>
2018-10-21 12:52:02 +0800

    init project: package and auto deploy and log-viewer and backup and so on

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Auto Deploy
                 </div>
 
                 <div class="links">
